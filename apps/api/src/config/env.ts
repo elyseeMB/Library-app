a5e5bc386757b3786d16c70f9315ee5e97f688d1
elysee <mboussaemmanuelito@gmail.com>
2026-09-13 01:42:55 +0100

    fix(api): simplify dotenv configuration

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -1,6 +1,3 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { config } from 'dotenv';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-config({ path: path.resolve(__dirname, '../../../../.env') });
+config();
