43a5e0c12cc86b0b8a760fd9c702b951c5defc04
elysee <mboussaemmanuelito@gmail.com>
2026-09-17 14:12:55 +0100

    refactor(api): make Kysely CLI independent from app config

--- apps/api/kysely.config.ts
+++ apps/api/kysely.config.ts
@@ -1,13 +1,24 @@
-import './src/config/env.ts';
+import { config } from 'dotenv';
+import { Kysely, PostgresDialect } from 'kysely';
 import { defineConfig } from 'kysely-ctl';
-import { db } from '#config/database';
+import pg from 'pg';
+
+config({ path: '../../.env' });
+
+const { Pool } = pg;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const db = new Kysely({
+  dialect: new PostgresDialect({
+    pool,
+  }),
+});
 
 export default defineConfig({
   kysely: db,
   migrations: {
-    migrationFolder: 'src/database/migrations',
-  },
-  seeds: {
-    seedFolder: 'src/database/seeds',
+    migrationFolder: './src/database/migrations',
   },
 });
